0959948194ce7807a804f7baaf5ef2c265ff7eca
yubin <shin.yubin18@gmail.com>
2024-05-02 19:59:31 +0900

    first commit

---
+++ src/main/java/com/soaf/soaf_be/SoafBeApplication.java
@@ -0,0 +1,13 @@
+package com.soaf.soaf_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoafBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoafBeApplication.class, args);
+	}
+
+}
