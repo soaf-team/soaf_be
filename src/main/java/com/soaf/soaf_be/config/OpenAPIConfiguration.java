db8d7393906db12c96b52405746ecbe128b4e5bb
yubin <shin.yubin18@gmail.com>
2024-05-19 22:01:01 +0900

    feat : 곰돌이 추가

--- src/main/java/com/soaf/soaf_be/config/OpenAPIConfiguration.java
+++ src/main/java/com/soaf/soaf_be/config/OpenAPIConfiguration.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenAPIConfiguration {
 
-    private static final String API_NAME = "Around Hub Studion";
-    private static final String API_VERSION = "1.0.0";
-    private static final String API_DESCRIPTION = "어라운드 허브 스튜디오 스프링 부트 레포지토리입니다.";
+    private static final String API_NAME = "SOAF";
+    private static final String API_VERSION = "0.0.0";
+    private static final String API_DESCRIPTION = "SOAF 스프링 부트 레포지토리입니다.";
 
     @Bean
     public OpenAPI OpenAPIConfig() {
